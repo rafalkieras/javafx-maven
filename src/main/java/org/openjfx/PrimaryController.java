e9fbf528937ce0adb37c86f9cefc12654190cd3d
Rafal Kieras <rafal.kieras@gmail.com>
2020-08-12 16:11:40 +0200

    Initial

---
+++ src/main/java/org/openjfx/PrimaryController.java
@@ -0,0 +1,13 @@
+package org.openjfx;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
